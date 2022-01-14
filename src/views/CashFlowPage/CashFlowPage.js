d1ce05afe8d949ba10d5b2f8318b574c3170db33
Kateryna Karpova <katyakarpova@gmail.com>
2022-01-14 23:24:08 +0100

    Create CashFlowPage.js

---
+++ src/views/CashFlowPage/CashFlowPage.js
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+export default function CashFlowPage() {
+  return (
+    <button>
+      <Link to="/reports">Go to ReportsPage</Link>
+    </button>
+  );
+}

558a525991dc4b1a89acc3cb4dfa85108e09ccf9
Kateryna Karpova <katyakarpova@gmail.com>
2022-01-14 23:23:27 +0100

    Update App.js
    Added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import HomePage from './views/HomePage/HomePage';
+import CashFlowPage from './views/CashFlowPage/CashFlowPage';
+import ReportsPage from './views/ReportsPage/ReportsPage';
 
 function App() {
-  return <p>React App</p>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/cashflow" element={<CashFlowPage />} />
+      <Route path="/reports" element={<ReportsPage />} />
+    </Routes>
+  );
 }
 
 export default App;

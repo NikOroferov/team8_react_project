f08bcfa56078a9c3b46ee2dd734ec5302bded6bd
Kateryna Karpova <katyakarpova@gmail.com>
2022-01-15 19:00:18 +0100

    Добавление стилей и имплементация в свитчер
    Добавила стили для разных брекпоинтов, переименовала файлы, добавила массивы с данными для тестирования компонента.
    Имплементировала свои компоненты в Настин свитчер

--- src/components/ReportsSwitcher/ReportsSwitcher.js
+++ src/components/ReportsSwitcher/ReportsSwitcher.js
@@ -1,51 +1,61 @@
-import 'ReportsSwitcher.css';
-// import CostsReport from '../CostsReport';
-// import IncomesReport from '../IncomesReport';
+import styles from './ReportsSwitcher.module.css';
+import CostsReport from '../CostsReport/CostsReport';
+import IncomesReport from '../IncomesReport/IncomesReport';
 import { useState } from 'react';
 
 export default function ReportsSwitcher() {
-    const [idxActiveReport, setIdxActiveReport] = useState(0);
+  const [idxActiveReport, setIdxActiveReport] = useState(0);
 
-    const reports = [{
-        name: "РАСХОДЫ"
-        // , element: <CostsReport />
-    }, {
-        name: "ДОХОДЫ"
-            // , element: <IncomesReport />
-        }];
+  const reports = [
+    {
+      name: 'РАСХОДЫ',
+      element: <CostsReport />,
+    },
+    {
+      name: 'ДОХОДЫ',
+      element: <IncomesReport />,
+    },
+  ];
 
-    const handleSwitchReport = (value) => {
-        switch (value) {
-            case 'next':
-                if (idxActiveReport === reports.length - 1) {
-                    setIdxActiveReport(0);
-                } else {
-                    setIdxActiveReport(idxActiveReport + 1);
-                };
-                break;
-            
-            case 'prev':
-                if (idxActiveReport === 0) {
-                    setIdxActiveReport(reports.length - 1);
-                } else {
-                    setIdxActiveReport(idxActiveReport - 1);
-                };
-                break;
-            
-            default: return;
+  const handleSwitchReport = value => {
+    switch (value) {
+      case 'next':
+        if (idxActiveReport === reports.length - 1) {
+          setIdxActiveReport(0);
+        } else {
+          setIdxActiveReport(idxActiveReport + 1);
         }
-    }
+        break;
 
-    return (
-        <div>
-            <div className='reportSwitcher'>
-            <button className='reportSwitcher__btn' type='button' onClick={() => handleSwitchReport("prev")}></button>
-            <p className='reportSwitcher__text'>{reports[idxActiveReport].name}</p>
-                <button className='reportSwitcher__btn' type='button' onClick={() => handleSwitchReport("next")}></button>
-            </div>
-            <div>
-                {/* {reports[idxActiveReport].element} */}
-            </div>
-        </div>
-    )
+      case 'prev':
+        if (idxActiveReport === 0) {
+          setIdxActiveReport(reports.length - 1);
+        } else {
+          setIdxActiveReport(idxActiveReport - 1);
+        }
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  return (
+    <section className={styles.sections}>
+      <div className={styles.reportSwitcher}>
+        <button
+          className={styles.reportSwitcher__btn}
+          type="button"
+          onClick={() => handleSwitchReport('prev')}
+        ></button>
+        <p className={styles.section_title}>{reports[idxActiveReport].name}</p>
+        <button
+          className={styles.reportSwitcher__btn}
+          type="button"
+          onClick={() => handleSwitchReport('next')}
+        ></button>
+      </div>
+      <div className={styles.container}>{reports[idxActiveReport].element}</div>
+    </section>
+  );
 }
--- src/views/ReportsPage/ReportsPage.js
+++ src/views/ReportsPage/ReportsPage.js
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import ReportsSwitcher from '../../components/ReportsSwitcher/ReportsSwitcher';
 
 export default function ReportsPage() {
   return (
-    <button>
-      <Link to="/cashflow">Вернуться на главную</Link>
-    </button>
+    <>
+      <button>
+        <Link to="/cashflow">Вернуться на главную</Link>
+      </button>
+      <ReportsSwitcher />
+    </>
   );
 }

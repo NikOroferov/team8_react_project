179b4a4cac441bf3a33d2c44dc45dc3e8b9b59d3
yuragms <yura.gms@gmail.com>
2022-01-15 22:38:57 +0200

    first version heder without logika

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import styles from './Header.module.css';
 import Logo from '../Logo/Logo';
+import Icons from '../../img/svg/sprite.svg';
+
+const name = 'icon-calculator';
 
 function Header() {
-    const isLoggedIn = true;
+  const isLoggedIn = true;
+
   return (
     <header>
       <div className={styles.header}>
-         <Logo />
-        <p>Хеадер</p>
-         {/* {isLoggedIn && <UserMenu />}  */}
+        <Logo />
+
+        {isLoggedIn && (
+          <div className={styles.userMenu}>
+            <div className={styles.firstLetter}>U</div>
+            <p className={styles.userName}>User Name</p>
+            <div className={styles.line}></div>
+            <button
+              type="button"
+              // onClick={toggleModal}
+              className={styles.logOutBtn}
+            >
+              <svg width="18" height="18">
+                <use xlinkHref={`${Icons}#icon-logout`} />
+              </svg>
+              {/* <Icons 
+         name='icon-calculator'
+    viewBox="0 0 32 32"
+         size='32'
+         
+       /> */}
+            </button>
+            <button className={styles.logOutTextBtn}>Выйти</button>
+          </div>
+        )}
       </div>
     </header>
   );

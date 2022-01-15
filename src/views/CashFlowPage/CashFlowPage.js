57e4c9f5aec5ef4ebd18be6369ab07cd2e9db61d
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2022-01-15 13:14:32 +0200

    start

--- src/views/CashFlowPage/CashFlowPage.js
+++ src/views/CashFlowPage/CashFlowPage.js
@@ -2,8 +2,13 @@ import { Link } from 'react-router-dom';
 
 export default function CashFlowPage() {
   return (
-    <button>
-      <Link to="/reports">Go to ReportsPage</Link>
-    </button>
+    <>
+      <button>
+        <Link to="/">Go to Home</Link>
+      </button>
+      <button>
+        <Link to="/reports">Go to ReportsPage</Link>
+      </button>
+    </>
   );
 }

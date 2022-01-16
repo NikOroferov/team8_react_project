e6633a9d89ad0693b5e65993129bb33f3b9b6af1
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2022-01-16 18:40:38 +0200

    tab

--- src/components/TableCashflo/TableCashflo.js
+++ src/components/TableCashflo/TableCashflo.js
@@ -1,10 +1,120 @@
-import { useMemo } from 'react';
-import { useTable } from 'react-table';
+import { useMemo, useCallback } from 'react';
+import { useTable, useBlockLayout } from 'react-table';
 // import styled from 'styled-components';
+import { FixedSizeList } from 'react-window';
+import scrollbarWidth from './scrollbarWidth';
 
 import { AiFillDelete } from 'react-icons/ai';
 
 import s from './TableCashflo.module.css';
+
+// const Styles = styled.div`
+//   /* padding: 1rem; */
+
+//   .table {
+//     display: inline-block;
+//     border-spacing: 0;
+//     /* border: 1px solid black; */
+
+//     .tr {
+//       :last-child {
+//         .td {
+//           border-bottom: 0;
+//         }
+//       }
+//     }
+
+//     .th,
+//     tableBox .td {
+//       margin: 0;
+//       padding: 0.5rem;
+//       border-bottom: 1px solid black;
+//       border-right: 1px solid black;
+
+//       :last-child {
+//         border-right: 1px solid black;
+//       }
+//     }
+//   }
+// `;
+
+function Table({ columns, data }) {
+  const defaultColumn = useMemo(
+    () => ({
+      width: 150,
+    }),
+    [],
+  );
+
+  const scrollBarSize = useMemo(() => scrollbarWidth(), []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    totalColumnsWidth,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+    },
+    useBlockLayout,
+  );
+
+  const RenderRow = useCallback(
+    ({ index, style }) => {
+      const row = rows[index];
+      prepareRow(row);
+      return (
+        <div
+          {...row.getRowProps({
+            style,
+          })}
+          className="tr"
+        >
+          {row.cells.map(cell => {
+            return (
+              <div {...cell.getCellProps()} className="td">
+                {cell.render('Cell')}
+              </div>
+            );
+          })}
+        </div>
+      );
+    },
+    [prepareRow, rows],
+  );
+
+  return (
+    <div {...getTableProps()} className="table">
+      <div>
+        {headerGroups.map(headerGroup => (
+          <div {...headerGroup.getHeaderGroupProps()} className="tr">
+            {headerGroup.headers.map(column => (
+              <div {...column.getHeaderProps()} className={s.thHeadTabl}>
+                {column.render('Header')}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div {...getTableBodyProps()}>
+        <FixedSizeList
+          height={400}
+          itemCount={rows.length}
+          itemSize={35}
+          width={totalColumnsWidth + scrollBarSize}
+        >
+          {RenderRow}
+        </FixedSizeList>
+      </div>
+    </div>
+  );
+}
 
 export default function TableCashflo() {
   const data = useMemo(
@@ -22,6 +132,27 @@ export default function TableCashflo() {
         col2: 'Купил опять',
         col3: 'Прочее',
         col4: '-20 000',
+        col5: <AiFillDelete />,
+      },
+      {
+        col1: '15.01.2022',
+        col2: 'Потратил',
+        col3: 'Прочее',
+        col4: '-2 000',
+        col5: <AiFillDelete />,
+      },
+      {
+        col1: '15.01.2022',
+        col2: 'Потратил',
+        col3: 'Прочее',
+        col4: '-2 000',
+        col5: <AiFillDelete />,
+      },
+      {
+        col1: '15.01.2022',
+        col2: 'Потратил',
+        col3: 'Прочее',
+        col4: '-2 000',
         col5: <AiFillDelete />,
       },
       {
@@ -110,68 +241,128 @@ export default function TableCashflo() {
     [],
   );
 
-  const tableInstance = useTable({ columns, data });
+  //   const defaultColumn = useMemo(
+  //     () => ({
+  //       width: 100,
+  //     }),
+  //     [],
+  //   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
+  //   const tableInstance = useTable({ columns, data, defaultColumn });
+
+  //   const scrollBarSize = useMemo(() => scrollbarWidth(), []);
+
+  //   const {
+  //     getTableProps,
+  //     getTableBodyProps,
+  //     headerGroups,
+  //     rows,
+  //     prepareRow,
+  //     totalColumnsWidth,
+  //   } = tableInstance;
+
+  //   const RenderRow = useCallback(
+  //     ({ index, style }) => {
+  //       const row = rows[index];
+  //       prepareRow(row);
+  //       return (
+  //         <div
+  //           {...row.getRowProps({
+  //             style,
+  //           })}
+  //           className="tr"
+  //         >
+  //           {row.cells.map(cell => {
+  //             return (
+  //               <div {...cell.getCellProps()} className="td">
+  //                 {cell.render('Cell')}
+  //               </div>
+  //             );
+  //           })}
+  //         </div>
+  //       );
+  //     },
+  //     [prepareRow, rows],
+  //   );
 
   return (
-    <div className={s.tabCashflow}>
-      {/* <Styles> */}
-      <table className={s.tableBox} {...getTableProps()}>
-        <thead className={s.headTabl}>
-          {
-            // Loop over the header rows
-            headerGroups.map(headerGroup => (
-              // Apply the header row props
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {
-                  // Loop over the headers in each row
-                  headerGroup.headers.map(column => (
-                    // Apply the header cell props
-                    <th {...column.getHeaderProps()}>
-                      {
-                        // Render the header
-                        column.render('Header')
-                      }
-                    </th>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </thead>
-        {/* Apply the table body props */}
-        <tbody {...getTableBodyProps()}>
-          {
-            // Loop over the table rows
-            rows.map(row => {
-              // Prepare the row for display
-              prepareRow(row);
-              return (
-                // Apply the row props
-                <tr className={s.lineBox} {...row.getRowProps()}>
-                  {
-                    // Loop over the rows cells
-                    row.cells.map(cell => {
-                      // Apply the cell props
-                      return (
-                        <td className={s.line} {...cell.getCellProps()}>
-                          {
-                            // Render the cell contents
-                            cell.render('Cell')
-                          }
-                        </td>
-                      );
-                    })
-                  }
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
-      {/* </Styles> */}
-    </div>
+    //   <Styles>
+
+    <Table columns={columns} data={data} />
+    // {
+    /* <div {...getTableProps()} className="table">
+        <div>
+          {headerGroups.map(headerGroup => (
+            <div {...headerGroup.getHeaderGroupProps()} className="tr">
+              {headerGroup.headers.map(column => (
+                <div {...column.getHeaderProps()} className="th">
+                  {column.render('Header')}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+
+        <div {...getTableBodyProps()}>
+          <FixedSizeList
+            height={400}
+            itemCount={rows.length}
+            itemSize={35}
+            width={totalColumnsWidth + scrollBarSize}
+          >
+            {RenderRow}
+          </FixedSizeList>
+        </div>
+      </div> */
+    // }
+    /* </Styles> */
   );
+
+  //   return (
+  //     <div className={s.tabCashflow}>
+  //       <Styles>
+  //         <FixedSizeList
+  //           height={300}
+  //           itemCount={rows.length}
+  //           itemSize={35}
+  //           width={totalColumnsWidth + scrollBarSize}
+  //         >
+  //           {RenderRow}
+  //         </FixedSizeList>
+  //         <table className={s.tableBox} {...getTableProps()}>
+  //           <thead className={s.headTabl}>
+  //             {headerGroups.map(headerGroup => (
+  //               <tr {...headerGroup.getHeaderGroupProps()}>
+  //                 {headerGroup.headers.map(column => (
+  //                   <th {...column.getHeaderProps()}>
+  //                     {
+  //                       // Render the header
+  //                       column.render('Header')
+  //                     }
+  //                   </th>
+  //                 ))}
+  //               </tr>
+  //             ))}
+  //           </thead>
+
+  //           <tbody {...getTableBodyProps()}>
+  //             {rows.map(row => {
+  //               prepareRow(row);
+  //               return (
+  //                 <tr className={s.lineBox} {...row.getRowProps()}>
+  //                   {row.cells.map(cell => {
+  //                     return (
+  //                       <td className={s.line} {...cell.getCellProps()}>
+  //                         {cell.render('Cell')}
+  //                       </td>
+  //                     );
+  //                   })}
+  //                 </tr>
+  //               );
+  //             })}
+  //           </tbody>
+  //         </table>
+  //       </Styles>
+  //     </div>
+  //   );
 }

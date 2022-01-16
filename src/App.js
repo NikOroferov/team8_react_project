6b20e386dcc1800b1e5343937b23a4fe37373197
Kateryna Karpova <katyakarpova@gmail.com>
2022-01-16 19:20:27 +0100

    Update App.js
    Добавила возможность создать вложенный раутинг

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './views/HomePage/HomePage';
 import CashFlowPage from './views/CashFlowPage/CashFlowPage';
 import ReportsPage from './views/ReportsPage/ReportsPage';
 import Header from './components/Header/Header';
+import Temporary from './components/TemporaryComponent/temporary';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cashflow" element={<CashFlowPage />} />
-        <Route path="/reports" element={<ReportsPage />} />
+        <Route path="/reports/*" element={<ReportsPage />} />
+        {/* <Route path="/reports/:category" element={<Temporary />} /> */}
       </Routes>
     </>
   );

6dc260c7d7c4073f3f0d0c00e24cd590974667b4
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2022-01-17 23:04:52 +0200

    components

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,14 +1,33 @@
-import css from './Balance.module.css';
+import { useState } from 'react';
+import ButtonGrey from '../Button/ButtonGrey';
+
+import s from './Balance.module.css';
 
 const Balance = () => {
+  const [balance, setBalance] = useState('00.00');
+
+  const handleChange = e => {
+    setBalance(e.target.value);
+  };
+
+  const clickBtn = e => {
+    e.preventDefault();
+    console.log('Подтвердить');
+  };
+
   return (
-    <form className={css.form}>
-      <label className={css.label}>
-        Баланс: <input className={css.input} type="number" value={'00.00'} />
+    <form className={s.forma}>
+      <label>
+        <span className={s.text}>Баланс:</span>
+        <input
+          className={s.summ}
+          type="number"
+          value={balance}
+          pattern="/^\s*-?[1-9]\d*(\.\d{1,2})?\s*$/"
+          onChange={handleChange}
+        />
       </label>
-      <button className={css.btn} type="submit">
-        подтвердить
-      </button>
+      <ButtonGrey name="ПОДТВЕРДИТЬ" type="button" click={clickBtn} />
     </form>
   );
 };

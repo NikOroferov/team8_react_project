e9cc6d868ac4322f5f5f9c62a0b366ed16dcb45e
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2022-01-18 00:26:01 +0200

    css fix

--- src/components/LinkToReports/LinkToReports.js
+++ src/components/LinkToReports/LinkToReports.js
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import { MdBarChart } from 'react-icons/md';
 
-import s from './LinkToReports.modele.css';
+import s from './LinkToReports.module.css';
 
 export default function LinkToReports() {
   return (
-    <Link className={s.reportsLink} to="/reports">
+    <Link to="/reports" className={s.reportsLink}>
       <span className={s.text}>Перейти к отчетам</span>
       <span className={s.icon}>
         <MdBarChart />

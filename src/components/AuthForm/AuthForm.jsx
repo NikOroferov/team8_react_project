0fcd02a31eb14ec08ccb3c0dbb929fda0cb556f1
yuragms <yura.gms@gmail.com>
2022-01-18 19:27:56 +0200

    formik validation 2button

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Auth.module.css';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function AuthForm() {
   const formik = useFormik({
@@ -8,11 +9,29 @@ export default function AuthForm() {
       email: '',
       password: '',
     },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Email is required'),
+      password: Yup.string()
+        .min(6, 'Password must be at least 6 characters.')
+        .required('Password is required'),
+    }),
+
     onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+      console.log('войти', values);
+      // alert(JSON.stringify(values, null, 2));
     },
   });
-  // console.log(email);
+
+  function handleButton() {
+    if (!Object.keys(formik.errors).length > 0) {
+      const registration = formik.values;
+      console.log('регистрация', registration);
+    }
+  }
+
+  console.log(!formik.errors);
   return (
     <div className={s.wrapper}>
       <div className={s.wrapperForm}>
@@ -24,7 +43,7 @@ export default function AuthForm() {
           </p>
           <form onSubmit={formik.handleSubmit} className={s.form}>
             <label className={s.label} htmlFor="email">
-              "Электронная почта"
+              Электронная почта
             </label>
             <input
               className={s.input}
@@ -35,8 +54,11 @@ export default function AuthForm() {
               onChange={formik.handleChange}
               value={formik.values.email}
             />
+            {formik.errors.email ? (
+              <p className={s.error}>{formik.errors.email}</p>
+            ) : null}
             <label className={s.label} htmlFor="password">
-              "Электронная почта"
+              Пароль
             </label>
             <input
               className={s.input}
@@ -44,14 +66,22 @@ export default function AuthForm() {
               name="password"
               type="password"
               onChange={formik.handleChange}
-              value={formik.values.firstName}
+              value={formik.values.password}
               placeholder="password"
             />
+            {formik.errors.password ? (
+              <p className={s.error}>{formik.errors.password}</p>
+            ) : null}
+
             <div className={s.butBox}>
               <button className={s.buttonAct} type="submit">
                 <span className={s.butTextAct}>Войти</span>
               </button>
-              <button className={s.buttonAct} type="submit">
+              <button
+                className={s.buttonAct}
+                type="button"
+                onClick={() => handleButton()}
+              >
                 <span className={s.butTextAct}>Регистрация</span>
               </button>
             </div>

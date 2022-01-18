b7641853ddedd05aa9a2c65c38810fb0e8cd4a4e
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2022-01-18 21:35:39 +0200

    баланс включил

--- src/components/ExpensesIncome/ExpensesIncome.js
+++ src/components/ExpensesIncome/ExpensesIncome.js
@@ -42,7 +42,7 @@ export default function ExpensesIncome() {
     <>
       <section className={s.section}>
         <div className={s.boxBalance}>
-          {/* <Balance /> */}
+          <Balance />
           <LinkToReports />
         </div>
 

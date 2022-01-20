74d5921014fc09f3c84e1285b96448a43efae443
Chantsova <stasyadiv@gmail.com>
2022-01-20 14:57:38 +0200

    Update ReportSwitcherBtns
    Update ReportSwitcherBtns

--- src/components/ReportsSwitcher/ReportsSwitcher.js
+++ src/components/ReportsSwitcher/ReportsSwitcher.js
@@ -37,13 +37,29 @@ export default function ReportsSwitcher({ handleTypeReport }) {
           className={styles.reportSwitcher__btn}
           type="button"
           onClick={()=>handleSwitchReport('prev')}
-        ></button>
+        > <svg
+          width="7"
+          height="12"
+          viewBox="0 0 7 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M6 1L2 6L6 11" stroke="#FF751D" strokeWidth="2" />
+        </svg></button>
         <p className={styles.section_title}>{typeName}</p>
         <button
           className={styles.reportSwitcher__btn}
           type="button"
           onClick={() => handleSwitchReport('next')}
-        ></button>
+        ><svg
+          width="7"
+          height="12"
+          viewBox="0 0 7 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1 1L5 6L1 11" stroke="#FF751D" strokeWidth="2" />
+        </svg></button>
       </div>
     </>
   );

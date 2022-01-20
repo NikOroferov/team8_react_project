248de17f4295562259510f06baa4a670536e94e3
Chantsova <stasyadiv@gmail.com>
2022-01-20 14:56:08 +0200

    Update ReportsPage to Transfer logic
    Update ReportsPage to Transfer logic

--- src/views/ReportsPage/ReportsPage.js
+++ src/views/ReportsPage/ReportsPage.js
@@ -1,51 +1,76 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import styles from './ReportsPage.module.css';
+import getCategoryReport from '../../services/api-services';
 import ReportsSwitcher from '../../components/ReportsSwitcher/ReportsSwitcher';
 import CurrentDateRaport from '../../components/CurrentDateRaport';
 import ExpensesComponent from '../../components/ExpensesComponent';
 import CostsReport from '../../components/CostsReport/CostsReport';
 import ReportsGraph from '../../components/ReportsGraph/ReportsGraph';
+import Background from '../Background/background';
+import LinkToMain from '../../components/LinkToMain/LinkToMain';
+import Balance from '../../components/Balance/Balance';
 
 export default function ReportsPage() {
-  const [typeReport, setTypeReport] = useState('');
-  const [date, setDate] = useState('');
-  const [categories, setCategories] = useState('');
-  const [subcategories, setSubcategories] = useState('');
+  const [typeReport, setTypeReport] = useState(null);
+  const [date, setDate] = useState(null);
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState(null);
+  const [firstCategory, setFirstCategory] = useState(null);
 
-  const queryReports = [date, typeReport];
-
-  useEffect(() => console.log(typeReport)
-    // // getReportsByDateAndType(queryReports)
+  useEffect(() => {
+    const queryReports = [date, typeReport];
+    // getCategoryReport(queryReports)
     //   .then(resp =>
-    //     setCategories(resp.data.categories))
-    , [date, typeReport]);
+    // setCategories(categories))
+
+    if (categories.length) {
+      setFirstCategory(categories[0].title);
+    } else {
+      return;
+    }
+  }, [date, typeReport, categories]);
 
   const handleDate = newDate => {
     setDate(newDate);
-   };
+  };
 
   const handleTypeReport = newTypeReport => {
     setTypeReport(newTypeReport);
   };
-  
-const handleSubcategories = (newSubcategories) => { 
+
+  const handleSubcategories = newSubcategories => {
     setSubcategories(newSubcategories);
   };
 
   return (
     <>
-      <button>
-        <Link to="/cashflow">Вернуться на главную</Link>
-      </button>
+      <Background>
+        <LinkToMain />
+        <Balance />
+        <CurrentDateRaport handleDate={handleDate} />
+        <ExpensesComponent />
+        <ReportsSwitcher
+          className={styles.reportSwitcher}
+          typeReport={typeReport}
+          handleTypeReport={handleTypeReport}
+        />
+        <CostsReport
+          categories={categories}
+          firstCategory={firstCategory}
+          typeReport={typeReport}
+          date={date}
+          handleSubcategories={handleSubcategories}
+        />
 
-      <CurrentDateRaport handleDate={handleDate}/>
-      <ExpensesComponent />
-      <ReportsSwitcher className={styles.reportSwitcher} typeReport={typeReport} handleTypeReport={handleTypeReport}
-      />
-      <CostsReport categories={categories} typeReport={typeReport} date={date} handleSubcategories={handleSubcategories} />
-      <ReportsGraph subcategories={subcategories} typeReport={typeReport} date={date}
-      />
+        {categories.length ? (
+          <ReportsGraph
+            subcategories={subcategories}
+            typeReport={typeReport}
+            date={date}
+          />
+        ) : null}
+      </Background>
     </>
   );
 }

6dd8b63d99c81a1480a8ca344df3e7322adf94c5
yuragms <yura.gms@gmail.com>
2022-01-22 17:48:15 +0200

    Update api-services.js

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'http://localhost:3001/api/';
 
 const baseUrl = axios.defaults.baseURL;
 // Token

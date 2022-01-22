a4c073320a51c9ed64f26a63667bbc48bbec9e41
Nikolay Yaryhin <77698141+NikOroferov@users.noreply.github.com>
2022-01-22 19:00:09 +0200

    fix import in redux

--- src/redux/auth/index.js
+++ src/redux/auth/index.js
@@ -1,4 +1,3 @@
-export { default as authActions } from './auth-actions';
-export { default as authReducer } from './auth-reducer';
 export { default as authOperations } from './auth-operations';
 export { default as authSelectors } from './auth-selectors';
+export { default as authReducer } from './auth-slice';
--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,7 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import authReducer from './auth/auth-slice';
+import { authReducer } from './auth';
 
 const authPersistConfig = {
   key: 'auth',

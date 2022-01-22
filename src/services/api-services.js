984cc3ad7f5bf4ba560f0fdcdbd0555ab65784ca
Nikolay Yaryhin <77698141+NikOroferov@users.noreply.github.com>
2022-01-22 20:33:20 +0200

    Update api-services.js

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'http://localhost:3001/api/';
 
 const baseUrl = axios.defaults.baseURL;
 
@@ -17,48 +17,48 @@ const token = {
 
 // Auth
 
-const register = requisites => axios.post('api/auth/register', requisites); //requisites={email,password}
+const register = requisites => axios.post('auth/register', requisites); //requisites={email,password}
 
-const login = requisites => axios.post('api/auth/login', requisites); //requisites={email,password}
+const login = requisites => axios.post('auth/login', requisites); //requisites={email,password}
 
-const logout = () => axios.post(`api/auth/logout`);
+const logout = () => axios.post(`auth/logout`);
 
 // const refresh = () => axios.get('auth/refresh');
 
 // User
 
-const setUserBalance = balance => axios.post('api/user/balance', balance);
-const getUserBalance = () => axios.get('api/user/balance');
-const updateUserBalance = balance => axios.patch('api/user/balance', balance);
-const getUserInfo = () => axios.get('api/user');
-const getCurrentUser = () => axios.get('api/user/current');
+const setUserBalance = balance => axios.post('user/balance', balance);
+const getUserBalance = () => axios.get('user/balance');
+const updateUserBalance = balance => axios.patch('user/balance', balance);
+const getUserInfo = () => axios.get('user');
+const getCurrentUser = () => axios.get('user/current');
 
 // Transactions
 
-const getTransaction = () => axios.get('/transaction');
+const getTransaction = () => axios.get('transaction');
 
 const addTransaction = transaction => axios.post('/transaction', transaction);
 
 const deleteTransaction = transactionId =>
   axios.delete(`/transaction/${transactionId}`);
 
-const getIncomes = () => axios.get('/transactionIncomes');
+const getIncomes = () => axios.get('transactionIncomes');
 
-const getExpenses = () => axios.get('/transactionExpenses');
+const getExpenses = () => axios.get('transactionExpenses');
 
 // Reports
 
 const getCategoryReport = (date, isIncome) =>
-  axios.get(`/transaction/category-by-month?date=${date}&isIncome=${isIncome}`);
+  axios.get(`transaction/category-by-month?date=${date}&isIncome=${isIncome}`);
 
 const getSubcategoryReport = (date, isIncome, category) =>
   axios.get(
-    `/transaction/subcategory-by-month?date=${date}&isIncome=${isIncome}&category=${category}`,
+    `transaction/subcategory-by-month?date=${date}&isIncome=${isIncome}&category=${category}`,
   );
 
-const getPeriodReports = () => axios.get('/transaction/period');
+const getPeriodReports = () => axios.get('transaction/period');
 
-const getResumeReport = date => axios.get(`/transaction/summary?date=${date}`);
+const getResumeReport = date => axios.get(`transaction/summary?date=${date}`);
 
 const api = {
   token,

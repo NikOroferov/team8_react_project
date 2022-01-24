eb5bb28858bc98ff5edac18ee5f1f78c6945c66f
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2022-01-24 20:29:18 +0200

    1

--- src/components/CashflowDataEntry/CashflowDataEntry.js
+++ src/components/CashflowDataEntry/CashflowDataEntry.js
@@ -11,7 +11,7 @@ import Button from '../Button/Button';
 import s from './CashflowDataEntry.module.css';
 import Icons from '../../img/svg/sprite.svg';
 // import toast from 'react-hot-toast';
-import { blue, red } from '@mui/material/colors';
+// import { blue, red } from '@mui/material/colors';
 
 // axios.defaults.headers.common = {
 //   Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWJlMGYxYmM3NjkxNTZlNjBkYTVmMiIsImlhdCI6MTY0Mjg3NDE0OSwiZXhwIjoxNjQ0MDgzNzQ5fQ.XDSTb16DBgzWSLYCWCQTVlJJkGbOEu1AUWzzzrHWK7U`,

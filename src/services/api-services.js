b2658d3016fd3ddd7b0904b48dd42c9cdb07abe9
yuragms <yura.gms@gmail.com>
2022-01-26 15:34:29 +0200

    1

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001/api/';
-// axios.defaults.baseURL = 'https://mongo-kapusta-team8.herokuapp.com/api/';
+// axios.defaults.baseURL = 'http://localhost:3001/api/';
+axios.defaults.baseURL = 'https://mongo-kapusta-team8.herokuapp.com/api/';
 
 const baseUrl = axios.defaults.baseURL;
 
